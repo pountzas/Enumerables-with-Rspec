a0cd836fe8852c892d8e97189ce20c8302c4f2fd
pountzas <nikos@pountzas.gr>
2021-04-01 00:33:17 +0300

    add my_each_with_index test cases

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -26,4 +26,23 @@ describe Enumerable do
       expect(range.my_each { |num| num }).to eql((1..10))
     end
   end
+
+  describe '#my_each_with_index' do
+    it 'if no block is given it returns an enumerator' do
+      expect(array.my_each_with_index.is_a?(Enumerable)).to eql(array.to_enum.is_a?(Enumerable))
+    end
+
+    it 'returns a hash with the index equel the value of key' do
+      %w[rock paper scissors].each_with_index { |item, index| hash[item] = index }
+      expect(hash).to eql({ 'rock' => 0, 'paper' => 1, 'scissors' => 2 })
+    end
+
+    it 'if the block is given returns an array' do
+      expect(range.my_each_with_index { |num| num }).to eql((1..10))
+    end
+
+    it 'if the block is given returns an array' do
+      expect(range.my_each_with_index { |num| num }).to_not eql([])
+    end
+  end
 end
