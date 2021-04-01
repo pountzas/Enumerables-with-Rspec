528cf52ba5d550430aae86d7d3e3da738ce2d37b
pountzas <nikos@pountzas.gr>
2021-04-02 00:21:52 +0300

    remove frozen string literal

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # rubocop: disable Metrics/MethodLength, Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
 # Implementing  ruby enum methods
--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # spec../enumerables
 
 # rubocop: disable Metrics/BlockLength
